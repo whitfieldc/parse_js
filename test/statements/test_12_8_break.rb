7928a31af0a7466358c7e42a482266ff964b990f
Rene Saarsoo <nene@triin.net>
2013-10-16 11:38:29 +0300

    Additional test for break.

---
+++ test/statements/test_12_8_break.rb
@@ -0,0 +1,46 @@
+require File.dirname(__FILE__) + "/../helper"
+
+class Statement_12_8_Break_Test < ExecuteTestCase
+  def setup
+    @runtime = RKelly::Runtime.new
+  end
+
+  def test_break_in_do_while_loop
+    assert_execute({ 'x' => 4 }, <<-EOJS)
+      var x = 0;
+      do {
+        if (x > 3) break;
+        x++;
+      } while (x < 10);
+    EOJS
+  end
+
+  def test_break_in_while_loop
+    assert_execute({ 'x' => 4 }, <<-EOJS)
+      var x = 0;
+      while (x < 10) {
+        if (x > 3) break;
+        x++;
+      }
+    EOJS
+  end
+
+  def test_break_in_for_loop
+    assert_execute({ 'x' => 4 }, <<-EOJS)
+      for (var x=0; x<10; x++) {
+        if (x > 3) break;
+      }
+    EOJS
+  end
+
+  def test_break_in_nested_loop
+    assert_execute({ 'x' => 10 }, <<-EOJS)
+      for (var x=0; x<10; x++) {
+        while (true) {
+          break;
+        }
+      }
+    EOJS
+  end
+
+end
