fa8e9bb2d24964ef640f746fd3192d951fe199f8
Rene Saarsoo <nene@triin.net>
2013-10-27 17:56:26 +0200

    Fix execution context test.

--- test/execution_contexts/test_10_1_3-1.rb
+++ test/execution_contexts/test_10_1_3-1.rb
@@ -3,9 +3,6 @@ require File.dirname(__FILE__) + "/../helper"
 class ExecutionContext_10_1_3_1_Test < Test::Unit::TestCase
   def setup
     @runtime = RKelly::Runtime.new
-    @runtime.define_function(:assert_equal) do |*args|
-      assert_equal(*args)
-    end
   end
 
   def test_myfun3_void_0
@@ -16,7 +13,7 @@ class ExecutionContext_10_1_3_1_Test < Test::Unit::TestCase
         var x = myfun3(2,4);
     EOJS
     assert env['x']
-    #assert_equal :undefined, scope_chain['x'].value
+    assert_equal :undefined, env['x'].value
   end
 
   def test_myfun3
