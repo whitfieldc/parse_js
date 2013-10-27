cd95da255f4ded6209f762b120e905db483b090b
Rene Saarsoo <nene@triin.net>
2013-10-27 19:45:20 +0200

    Eliminate bunch of unused JS::* classes.

--- lib/rkelly/js/number.rb
+++ lib/rkelly/js/number.rb
@@ -1,21 +1,13 @@
 module RKelly
   module JS
     class Number < Base
-      class << self
-        def create(*args)
-          self.new(args.first || 0)
-        end
-      end
-
-      def initialize(value = 0)
+      def initialize()
         super()
         self['MAX_VALUE'] = VALUE[1.797693134862315e+308]
         self['MIN_VALUE'] = VALUE[1.0e-306]
         self['NaN']       = VALUE[JS::NaN.new]
         self['POSITIVE_INFINITY'] = VALUE[1.0/0.0]
         self['NEGATIVE_INFINITY'] = VALUE[-1.0/0.0]
-        self['valueOf'] = VALUE[lambda { value }]
-        self['toString'] = VALUE[value.to_s]
       end
     end
   end
--- lib/rkelly/js/string.rb
+++ lib/rkelly/js/string.rb
@@ -1,12 +1,6 @@
 module RKelly
   module JS
     class String < Base
-      class << self
-        def create(*args)
-          self.new(args.first || '')
-        end
-      end
-
       def initialize()
         super()
         self['fromCharCode'] = VALUE[RubyFunction.new do |this, *args|
